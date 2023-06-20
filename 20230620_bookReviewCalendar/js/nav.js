7b7fa210f8a5a57d80359eb0b9e44f50f78b1a1c
Seoyeon0203 <w2222@e-mirim.hs.kr>
2023-06-20 12:19:12 +0900

    bookshelf css, js, html

--- 20230620_bookReviewCalendar/js/nav.js
+++ 20230620_bookReviewCalendar/js/nav.js
@@ -1,49 +1,41 @@
-// function sayHello(){
+// function sayHello() {
 //     alert("안녕");
 // }
 // sayHello();
 
-// 1. HTML -> js 
-// 햄버거메뉴 -> navToggleDiv 
-// 햄버거메뉴 i - > navToggleI
-// .nav-list -> navListUl
+// 1. HTML -> js
+//  햄버거메뉴 -> navToggleDiv
+//  햄버거메뉴 i -> navToggleI
+//  .nav-list -> navListUl
 // const navToggleDiv = document.getElementById("nav_toggle");
-const navToggleI = nav_toggle.getElemntsByTagName("i")[0];
+const navToggleI = nav_toggle.getElementsByTagName("i")[0];
 const navListUl = document.getElementsByClassName("nav-list")[0];
 
 // 2. Event Handling
-// nav_toggle.onclick =  () =>  alert("안녕"); //   ok 익명함수(람다식)
-// nav_toggle.onclick =   alert("안녕"); // not ok
-nav_toggle.onclick =() => {
-    // 햄버거메뉴 클릭하면, 햄버거메뉴 i "bi bi-list<-> X "bi bi -x-lg"
+// nav_toggle.onclick = () => alert("안녕");   // ok
+// nav_toggle.onclick = alert("안녕");         // not ok
+nav_toggle.onclick = () => {
+    //  햄버거메뉴 클릭하면, 햄버거메뉴 i "bi bi-list" <-> X "bi bi-x-lg"
     navToggleI.classList.toggle("bi-list");
     navToggleI.classList.toggle("bi-x-lg");
 
-    // 햄버거메뉴 클릭하면, .nav-list에 .show-menu 보여주기 <-> 안 보여주기
+    //  햄버거메뉴 클릭하면, .nav-list에 .show-menu 보여주기 <-> 안보여주기
     navListUl.classList.toggle("show-menu");
-
 }
 
-// ---------------------------------
-// HTML -> js
-// id 
+//---------------------------------
+//HTML -> js
+//id
 // const navToggleDiv = document.getElementById("nav_toggle");
-// const navToggleDiv = nav_toggle; // js 변수규칙은 지켜져야 함 
+// nav_toggle  //js 변수규칙은 지켜져야 함
 
-// class
+//class
 // const navToggleDiv = document.getElementsByClassName("nav-toggle")[0];
 
-// tag
+//tag
 // const navToggleDiv = document.getElementsByTagName("div")[0];
 
-// querySelector, querySelectorAll
+//querySelector, querySelectorAll
 // const navToggleDiv = document.querySelector("#nav_toggle");
 // const navToggleDiv = document.querySelectorAll(".nav-toggle")[0];
 // const navToggleDiv = document.querySelectorAll("div")[0];
-
-
-
-
-
-
-   
