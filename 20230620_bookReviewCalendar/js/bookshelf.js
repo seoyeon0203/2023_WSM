e87e6a10a289b215ae52a16b8271a1d57b446f05
Seoyeon0203 <w2222@e-mirim.hs.kr>
2023-06-20 12:24:50 +0900

    bookshelf css, js, html

--- 20230620_bookReviewCalendar/js/bookshelf.js
+++ 20230620_bookReviewCalendar/js/bookshelf.js
@@ -2,10 +2,10 @@
 
 // HTML -> js
 const bookItems = document.getElementsByClassName("book-item");
-let i = 0;
-for(let bookItem of bookItems){
-    const bookItemImg = bookItem.getElementsByClassName("img")[0]; // .book-item 하나씩 꺼내어 img 태그 가져오기
-    bookItemImg.src= books[i].img; // data.js에서 img 값 가져와서 img 태그에 적용하기 
+let i = 0
+for (let bookItem of bookItems) {
+    const bookItemImg = bookItem.getElementsByTagName("img")[0];    //.book-item 하나씩 꺼내어 img 태그 가져오자
+    bookItemImg.src = books[i].img;    //data.js 에서 img 값 가져와서 img 태그에 적용하자
     i++;
 }
 
